5448078431a29885495a86ad0c041069715d8d94
“Deddoctor30”~ <alinsbomb@yandex.ru>
2023-09-20 11:27:53 +0300

    add new commit

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,13 +1,13 @@
-import styles from './About.module.scss';
+import styles from "./About.module.scss";
 
 export default function About() {
-   return (
-     <div className={styles.about}>
-       <h2>About</h2>
-       <h2>About123</h2>
-       <h2>About123</h2>
-       <h2>About</h2>
-       <h2>About</h2>
-     </div>
-   );
- }
+  return (
+    <div className={styles.about}>
+      <h2>About</h2>
+      <h2>About123</h2>
+      <h2>About123</h2>
+      <h2>About</h2>
+      <h2>About</h2>
+    </div>
+  );
+}
